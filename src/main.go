4735e522e1e442fe06ba0acd2fb4db0f29019731
Jonathan Atta <50656434+Laodeus@users.noreply.github.com>
2023-05-25 14:58:17 +0200

    Revert "adding docker db, creating user registration"
    This reverts commit 6dd99e4f99c4c415bc1d2f2b6ad72034d42b453d.

--- src/main.go
+++ src/main.go
@@ -8,23 +8,18 @@ import (
 )
 
 func main() {
+
 	// load the env variables
 	LoadEnv()
 
 	port := os.Getenv("SERVER_PORT")
-	log.Println(port)
 
-	// connect to db
-	db, err := getDb()
-	if err != nil {
-		fmt.Println("Error creating database connection :", err)
-		return
-	}
-	defer db.Close()
-
+	// definition des routes
 	router := Routes()
 
-	fmt.Println(fmt.Sprintf("Server started at port %s", port))
+	// Message indiquant que le serveur a démarré
+	fmt.Println(fmt.Sprintf("Le serveur a démarré sur le port %s", port))
 
+	// Démarrage du serveur
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
 }
--- src/routes.go
+++ src/routes.go
@@ -8,8 +8,5 @@ func Routes() *http.ServeMux {
 
 	router := http.NewServeMux()
 
-	// Route POST /api/v1/users/register
-	router.HandleFunc("/api/v1/users/register", RegisterUser)
-
 	return router
 }
