6f9662478f61dce3e01c00d442cceac2274bc319
Jonathan Atta <50656434+Laodeus@users.noreply.github.com>
2023-05-26 17:50:58 +0200

    adding post new vehicules done

---
+++ routes/vehicules/postNewVehicules.go
@@ -0,0 +1,42 @@
+package routesVehicules
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Laodeus/glt/utils"
+)
+
+func PostNewVehicules(responseWriter http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		responseWriter.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var vehicule Vehicules
+	err := json.NewDecoder(request.Body).Decode(&vehicule)
+	if err != nil {
+		responseWriter.WriteHeader(http.StatusBadRequest)
+		responseWriter.Write([]byte("bad body"))
+		return
+	}
+
+	db, err := utils.GetDb()
+	if err != nil {
+		fmt.Println("Error creating database connection :", err)
+		return
+	}
+
+	defer db.Close()
+
+	_, err = db.Db.Exec("INSERT INTO  vehicules (name, type) VALUES ($1, $2)", vehicule.Name, vehicule.Type)
+	if err != nil {
+		responseWriter.WriteHeader(http.StatusConflict)
+		responseWriter.Write([]byte(err.Error()))
+		return
+	} else {
+		responseWriter.WriteHeader(http.StatusOK)
+		responseWriter.Write([]byte("Vehicule added"))
+	}
+}
